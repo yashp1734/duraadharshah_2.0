911aa8e24d543d80c5353903987562634d5fca86
Yash Hitendra Patel <yashp1734@gmail.com>
2024-03-03 14:40:27 +0530

    upadated

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
 
-// function validateHTML($html) {
-//     $validator_url = 'https://validator.w3.org/nu/';
-//     $data = [
-//         'out' => 'json',
-//         'content' => $html,
-//     ];
+function validateHTML($html) {
+    $validator_url = 'https://validator.w3.org/nu/';
+    $data = [
+        'out' => 'json',
+        'content' => $html,
+    ];
 
-//     $options = [
-//         'http' => [
-//             'method'  => 'POST',
-//             'header'  => 'Content-type: application/x-www-form-urlencoded',
-//             'content' => http_build_query($data),
-//         ],
-//     ];
+    $options = [
+        'http' => [
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => http_build_query($data),
+        ],
+    ];
 
-//     $context  = stream_context_create($options);
+    $context  = stream_context_create($options);
 
-//     $result = file_get_contents($validator_url, false, $context);
+    $result = file_get_contents($validator_url, false, $context);
 
-//     if ($result === false) {
-//         // Error handling
-//         return "Error accessing the validation service.";
-//     }
+    if ($result === false) {
+        // Error handling
+        return "Error accessing the validation service.";
+    }
 
-//     return $result;
-// }
+    return $result;
+}
 
-// $html = file_get_contents('index.html');
-// $validationResult = validateHTML($html);
-// echo $validationResult;
+$html = file_get_contents('index.html');
+$validationResult = validateHTML($html);
+echo $validationResult;
